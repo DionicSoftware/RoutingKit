0cb1617d2df344a1e833c38dd1f9268236a8431b
Null3rror <null3rr0r@protonmail.com>
2023-04-28 11:52:02 +0200

    Finish RoutingKit wrapper for Windows and Linux

--- src/routingkit_wrapper.cpp
+++ src/routingkit_wrapper.cpp
@@ -1,22 +1,12 @@
 #include <routingkit_wrapper.h>
 
 
-ROUTINGKIT_API ContractionHierarchy* build_contraction_hierarchy(unsigned node_count, unsigned edge_count, unsigned tail[], unsigned head[], unsigned weight[], bool* result) {
+ROUTINGKIT_API ContractionHierarchy* build_contraction_hierarchy(unsigned node_count, unsigned edge_count, unsigned tail[], unsigned head[], unsigned weight[]) {
     vector<unsigned> tail_vector(tail, tail + edge_count);
     vector<unsigned> head_vector(head, head + edge_count);
     vector<unsigned> weight_vector(weight, weight + edge_count);
     ContractionHierarchy* contraction_hierarchy = new ContractionHierarchy();
-    try
-    {
-        *contraction_hierarchy = ContractionHierarchy::build(node_count, tail_vector, head_vector, weight_vector);
-        *result = true;
-    }
-    catch(...)
-    {
-        *result = false;
-    }
-    
-    
+    *contraction_hierarchy = ContractionHierarchy::build(node_count, tail_vector, head_vector, weight_vector);
     return contraction_hierarchy;
 }
 
@@ -56,7 +46,7 @@ ROUTINGKIT_API unsigned contraction_hierarchy_query_get_distance(ContractionHier
 ROUTINGKIT_API unsigned* contraction_hierarchy_query_get_node_path(ContractionHierarchyQuery* contraction_hierarchy_query, unsigned* num_of_nodes) {
     vector<unsigned> node_path_vector = contraction_hierarchy_query->get_node_path();
     *num_of_nodes = node_path_vector.size();
-    unsigned* node_path = new unsigned[*num_of_nodes];
+    unsigned* node_path = new unsigned[node_path_vector.size()];
     std::copy(node_path_vector.begin(), node_path_vector.end(), node_path);
     return node_path;
 }
