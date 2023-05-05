19f038501cc101ad1728feedb2945036adf18754
Null3rror <null3rr0r@protonmail.com>
2023-05-05 14:34:09 +0200

    Remove using namespaces

--- include/routingkit_wrapper.h
+++ include/routingkit_wrapper.h
@@ -1,29 +1,33 @@
+#ifndef ROUTINGKIT_WRAPPER
+#define ROUTINGKIT_WRAPPER
+
 #include <routingkit/contraction_hierarchy.h>
 #include <vector>
 
-using namespace std;
-using namespace RoutingKit;
 
-#if defined(_MSC_VER)
+#ifdef _MSC_VER
+#pragma once
 #ifdef ROUTINGKIT_EXPORTS
 /* We are building this library */
 #   define ROUTINGKIT_API extern "C" __declspec(dllexport)
 #else
-/* We are using this library */ 
+/* We are using this library */
 #   define ROUTINGKIT_API extern "C" __declspec(dllimport)
 #endif
 #else
-    #define ROUTINGKIT_API extern "C"  
+    #define ROUTINGKIT_API extern "C"
 #endif
 
-ROUTINGKIT_API ContractionHierarchy* build_contraction_hierarchy(unsigned node_count, unsigned edge_count, unsigned tail[], unsigned head[], unsigned weight[]);
-ROUTINGKIT_API void free_contraction_hierarchy(ContractionHierarchy* contraction_hierarchy);
-ROUTINGKIT_API ContractionHierarchyQuery* build_contraction_hierarchy_query(ContractionHierarchy* contraction_hierarchy);
-ROUTINGKIT_API void free_contraction_hierarchy_query(ContractionHierarchyQuery* contraction_hierarchy_query);
-ROUTINGKIT_API void reset_contraction_hierarchy_query(ContractionHierarchyQuery* contraction_hierarchy_query);
-ROUTINGKIT_API void contraction_hierarchy_query_add_source(ContractionHierarchyQuery* contraction_hierarchy_query, unsigned source);
-ROUTINGKIT_API void contraction_hierarchy_query_add_target(ContractionHierarchyQuery* contraction_hierarchy_query, unsigned target);
-ROUTINGKIT_API void run_contraction_hierarchy_query(ContractionHierarchyQuery* contraction_hierarchy_query);
-ROUTINGKIT_API unsigned contraction_hierarchy_query_get_distance(ContractionHierarchyQuery* contraction_hierarchy_query);
-ROUTINGKIT_API unsigned* contraction_hierarchy_query_get_node_path(ContractionHierarchyQuery* contraction_hierarchy_query, unsigned* num_of_nodes);
+ROUTINGKIT_API RoutingKit::ContractionHierarchy* build_contraction_hierarchy(unsigned node_count, unsigned edge_count, unsigned tail[], unsigned head[], unsigned weight[]);
+ROUTINGKIT_API void free_contraction_hierarchy(RoutingKit::ContractionHierarchy* contraction_hierarchy);
+ROUTINGKIT_API RoutingKit::ContractionHierarchyQuery* build_contraction_hierarchy_query(RoutingKit::ContractionHierarchy* contraction_hierarchy);
+ROUTINGKIT_API void free_contraction_hierarchy_query(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query);
+ROUTINGKIT_API void reset_contraction_hierarchy_query(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query);
+ROUTINGKIT_API void contraction_hierarchy_query_add_source(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query, unsigned source);
+ROUTINGKIT_API void contraction_hierarchy_query_add_target(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query, unsigned target);
+ROUTINGKIT_API void run_contraction_hierarchy_query(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query);
+ROUTINGKIT_API unsigned contraction_hierarchy_query_get_distance(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query);
+ROUTINGKIT_API unsigned* contraction_hierarchy_query_get_node_path(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query, unsigned* num_of_nodes);
 ROUTINGKIT_API void contraction_hierarchy_query_free_node_path(unsigned* node_path);
+
+#endif
--- src/routingkit_wrapper.cpp
+++ src/routingkit_wrapper.cpp
@@ -1,50 +1,50 @@
 #include <routingkit_wrapper.h>
 
 
-ROUTINGKIT_API ContractionHierarchy* build_contraction_hierarchy(unsigned node_count, unsigned edge_count, unsigned tail[], unsigned head[], unsigned weight[]) {
-    vector<unsigned> tail_vector(tail, tail + edge_count);
-    vector<unsigned> head_vector(head, head + edge_count);
-    vector<unsigned> weight_vector(weight, weight + edge_count);
-    ContractionHierarchy* contraction_hierarchy = new ContractionHierarchy();
-    *contraction_hierarchy = ContractionHierarchy::build(node_count, tail_vector, head_vector, weight_vector);
+ROUTINGKIT_API RoutingKit::ContractionHierarchy* build_contraction_hierarchy(unsigned node_count, unsigned edge_count, unsigned tail[], unsigned head[], unsigned weight[]) {
+    std::vector<unsigned> tail_vector(tail, tail + edge_count);
+    std::vector<unsigned> head_vector(head, head + edge_count);
+    std::vector<unsigned> weight_vector(weight, weight + edge_count);
+    RoutingKit::ContractionHierarchy* contraction_hierarchy = new RoutingKit::ContractionHierarchy();
+    *contraction_hierarchy = RoutingKit::ContractionHierarchy::build(node_count, tail_vector, head_vector, weight_vector);
     return contraction_hierarchy;
 }
 
-ROUTINGKIT_API void free_contraction_hierarchy(ContractionHierarchy* contraction_hierarchy) {
+ROUTINGKIT_API void free_contraction_hierarchy(RoutingKit::ContractionHierarchy* contraction_hierarchy) {
     delete contraction_hierarchy;
 }
 
-ROUTINGKIT_API ContractionHierarchyQuery* build_contraction_hierarchy_query(ContractionHierarchy* contraction_hierarchy) {
-    ContractionHierarchyQuery* contraction_hierarchy_query = new ContractionHierarchyQuery(*contraction_hierarchy);
+ROUTINGKIT_API RoutingKit::ContractionHierarchyQuery* build_contraction_hierarchy_query(RoutingKit::ContractionHierarchy* contraction_hierarchy) {
+    RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query = new RoutingKit::ContractionHierarchyQuery(*contraction_hierarchy);
     return contraction_hierarchy_query;
 }
 
-ROUTINGKIT_API void free_contraction_hierarchy_query(ContractionHierarchyQuery* contraction_hierarchy_query) {
+ROUTINGKIT_API void free_contraction_hierarchy_query(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query) {
     delete contraction_hierarchy_query;
 }
 
-ROUTINGKIT_API void reset_contraction_hierarchy_query(ContractionHierarchyQuery* contraction_hierarchy_query) {
+ROUTINGKIT_API void reset_contraction_hierarchy_query(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query) {
     contraction_hierarchy_query->reset();
 }
 
-ROUTINGKIT_API void contraction_hierarchy_query_add_source(ContractionHierarchyQuery* contraction_hierarchy_query, unsigned source) {
+ROUTINGKIT_API void contraction_hierarchy_query_add_source(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query, unsigned source) {
     contraction_hierarchy_query->add_source(source);
 }
 
-ROUTINGKIT_API void contraction_hierarchy_query_add_target(ContractionHierarchyQuery* contraction_hierarchy_query, unsigned target) {
+ROUTINGKIT_API void contraction_hierarchy_query_add_target(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query, unsigned target) {
     contraction_hierarchy_query->add_target(target);
 }
 
-ROUTINGKIT_API void run_contraction_hierarchy_query(ContractionHierarchyQuery* contraction_hierarchy_query) {
+ROUTINGKIT_API void run_contraction_hierarchy_query(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query) {
     contraction_hierarchy_query->run();
 }
 
-ROUTINGKIT_API unsigned contraction_hierarchy_query_get_distance(ContractionHierarchyQuery* contraction_hierarchy_query) {
+ROUTINGKIT_API unsigned contraction_hierarchy_query_get_distance(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query) {
     return contraction_hierarchy_query->get_distance();
 }
 
-ROUTINGKIT_API unsigned* contraction_hierarchy_query_get_node_path(ContractionHierarchyQuery* contraction_hierarchy_query, unsigned* num_of_nodes) {
-    vector<unsigned> node_path_vector = contraction_hierarchy_query->get_node_path();
+ROUTINGKIT_API unsigned* contraction_hierarchy_query_get_node_path(RoutingKit::ContractionHierarchyQuery* contraction_hierarchy_query, unsigned* num_of_nodes) {
+    std::vector<unsigned> node_path_vector = contraction_hierarchy_query->get_node_path();
     *num_of_nodes = node_path_vector.size();
     unsigned* node_path = new unsigned[node_path_vector.size()];
     std::copy(node_path_vector.begin(), node_path_vector.end(), node_path);
